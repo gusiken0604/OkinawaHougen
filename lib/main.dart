e58782fad92f1c694ea19f4400f4b2136bb0478e
Gushiken <gusiken0604@gmail.com>
2023-09-30 05:36:16 +0900

    コピペミス修正

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget git@github.com:gusiken0604/okinawahougen.git
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
