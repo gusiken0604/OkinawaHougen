22214e8fb877a91feddaa8cd201595f6949490a0
Gushiken <gusiken0604@gmail.com>
2023-10-03 12:37:28 +0900

    カラム修正、リスト表示

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -9,33 +9,69 @@ import 'package:flutter/services.dart';
 Database? db; // データベースを保持するためのグローバル変数
 
 Future<void> initDb() async {
-  var databasesPath = await getDatabasesPath();
-  var path = join(databasesPath, "assets/okinawahougen.db");
+  try {
+    var databasesPath = await getDatabasesPath();
+    var path = join(databasesPath, "okinawahougen.db"); //assets/okinawahougen.db"
 
-  var exists = await databaseExists(path);
+    var exists = await databaseExists(path);
 
-  if (!exists) {
-    // DBが存在しない場合の処理
-    // アプリケーションを最初に起動したときのみ発生するはずです
-    print("アプリケーションを最初に起動したときのみ発生Creating new copy from asset");
-// 親ディレクトリが存在することを確認する
-    try {
-      await Directory(dirname(path)).create(recursive: true);
-    } catch (_) {}
-    // アセットからコピー
-    ByteData data = await rootBundle.load(join("assets", "example.db"));
-    List<int> bytes =
-        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    if (!exists) {
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
 
-// 書き込まれたバイトを書き込み、フラッシュする
-    await File(path).writeAsBytes(bytes, flush: true);
-  } else {
-    // DBが存在する場合の処理
-    print("DBが存在する場合の処理Opening existing database");
+      ByteData data = await rootBundle.load(join("assets", "okinawahougen.db"));
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+      await File(path).writeAsBytes(bytes, flush: true);
+    }
+
+    db = await openDatabase(path, readOnly: true);
+    print("56initDbが完了しました");
+
+  } catch (e) {
+    print("initDb エラー: $e");
   }
-  // DBファイルを開く
-  db = await openDatabase(path, readOnly: true);
 }
+
+Future<void> readDatabase() async {
+  if (db != null) {
+    // すでにデータベースが開かれていると仮定（dbはnullでないと仮定）
+    final List<Map<String, dynamic>> maps = await db!.query('newHougen1001');
+
+    for (var i = 0; i < maps.length; i++) {
+      // これで各レコードがコンソールに出力されます
+    }
+  }
+}
+
+Future<List<Map<String, dynamic>>> fetchDataFromDatabase() async {
+  List<Map<String, dynamic>> maps = [];
+  if (db != null) {
+    maps = await db!.query('newHougen1001');
+  }
+  return maps;
+}
+
+// Future<String> fetchDataFromDatabase() async {
+//   if (db != null) {
+//     final List<Map<String, dynamic>> maps = await db!.query('newHougen1001');
+//     print("取得したデータ: $maps");
+//     if (maps.isNotEmpty) {
+//       print("取得した最初のレコードのカラム名: ${maps[0].keys}");  // この行を追加
+//
+//       String data = "";
+//       for (var i = 0; i < maps.length; i++) {
+//         data += "id: ${maps[i]['id'] ?? 'null'}, hougen: ${maps[i]['hougen'] ?? 'null'}\n";
+//       }
+//       return data;
+//     } else {
+//       return "データベースは空です";
+//     }
+//   } else {
+//     return "データベースが初期化されていません";
+//   }
+// }
 
 class QuizPage extends StatefulWidget {
   @override
@@ -43,17 +79,43 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Map<String, dynamic>>? databaseData;
+
   @override
   void initState() {
     super.initState();
-    initDb(); // initState内で非同期処理を呼び出す
+    initDb().then((_) async {
+      List<Map<String, dynamic>> fetchedData = await fetchDataFromDatabase();
+      setState(() {
+        databaseData = fetchedData;
+      });
+    }).catchError((error) {
+      print("initState エラー: $error");
+    });
   }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   initDb().then((_) async { // initDbが完了したら
+  //     String fetchedData = await fetchDataFromDatabase(); // データベースを読む
+  //     print("UIに表示するデータ: $fetchedData");  // この行を追加
+  //     setState(() {
+  //       databaseData = fetchedData; // 状態を更新
+  //     });
+  //   }).catchError((error) {
+  //     print("initState エラー: $error");
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         title: Text(
           '沖縄方言',
           style: TextStyle(
@@ -61,30 +123,44 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
       ),
-      body: Center(),
+      body: databaseData == null
+          ? CircularProgressIndicator()
+          : ListView.builder(
+        itemCount: databaseData!.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(databaseData![index]['hougen'] ?? ''),
+            subtitle: Text(databaseData![index]['japanese'] ?? ''),
+          );
+        },
+      ),
     );
   }
 }
 
-// class QuizPage extends StatelessWidget {
-//   const QuizPage({Key? key}) : super(key: key);
+//   String? databaseData; // データベースのデータを格納する変数
 //
 //   @override
 //   Widget build(BuildContext context) {
-//
-//
 //     return Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//           title: Text(
-//             '沖縄方言',
-//             style: TextStyle(
-//               fontSize: 40,
-//             ),
+//       appBar: AppBar(
+//         backgroundColor: Theme
+//             .of(context)
+//             .colorScheme
+//             .inversePrimary,
+//         title: Text(
+//           '沖縄方言',
+//           style: TextStyle(
+//             fontSize: 40,
 //           ),
 //         ),
-//       body: Center(
-//              ),
+//       ),
+//       body: databaseData == null ?
+//       CircularProgressIndicator() : // ロード中
+//       Text(
+//         'データ:\n$databaseData',
+//         style: TextStyle(fontSize: 16),
+//       ),
 //     );
 //   }
 // }
